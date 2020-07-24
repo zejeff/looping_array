8be10dd99c959fec6c996b00d8d1077c29a20b1e
Jeffry <jeffryfleischer@gmail.com>
2020-07-23 19:19:16 -0700

    added and tested number collection

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+// input to collect user data
+const userNumber = parseInt($('#input1').val());
+console.log(userNumber);
+console.log("#input1");
+
+//loop to parse data into an array
+// let numberList = for(let index = 1; index <= userNumber; index += 1) {
+  
+// }
+//loop to prase array into new array == #'s into words
+});
